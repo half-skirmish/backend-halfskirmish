d170f3396df20199ef3f592305dc4b4f84a919ec
naman-1905 <namanoj19@gmail.com>
2025-08-25 21:19:19 +0530

    Added a create post button to the Topbar

--- app/components/dashboard/Topbar.js
+++ app/components/dashboard/Topbar.js
@@ -1,23 +1,16 @@
 export default function Topbar() {
   return (
-    <div className="bg-gray-800 h-16 fixed right-0 top-0 left-64 px-6 flex items-center justify-between">
-      <div className="text-xl font-semibold text-white">Dashboard</div>
+    <div className="bg-white h-16 fixed right-0 top-0 left-64 px-6 border border-[#F3F2F1] flex items-center justify-between">
+      <div className="text-xl font-semibold text-[#0078D4]">Admin Panel</div>
       
       <div className="flex items-center space-x-4">
-        <button className="text-gray-300 hover:text-white">
-          <span className="sr-only">Notifications</span>
-          {/* Add notification icon */}
-        </button>
         
         <div className="relative">
-          <button className="flex items-center space-x-2 text-gray-300 hover:text-white">
-            <img
-              src="/avatar-placeholder.png"
-              alt="User avatar"
-              className="h-8 w-8 rounded-full"
-            />
-            <span>John Doe</span>
-          </button>
+
+        <button className="text-white bg-[#0078D4] hover:bg-[#005ba0] px-3 py-3 rounded-lg text-sm">
+          + Create Post
+        </button>
+
         </div>
       </div>
     </div>
