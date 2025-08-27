d1c2029f7a5fbf0230efbe1ab465ce9576ed7ab6
naman-1905 <namanoj19@gmail.com>
2025-08-28 01:35:49 +0530

    Updated the Dashboard

--- app/Screens/Dashboard.js
+++ app/Screens/Dashboard.js
@@ -5,9 +5,9 @@ import MainContent from '../components/dashboard/MainContent';
 export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-900">
-      <Sidebar />
       <Topbar />
-      <MainContent />
+      <Sidebar />
+      <MainContent/>
     </div>
   );
 }
