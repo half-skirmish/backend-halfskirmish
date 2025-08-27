e2626604c56a373e5c85415ee3afc3bfb69938bc
naman-1905 <namanoj19@gmail.com>
2025-08-28 01:36:07 +0530

    Updated the MainContent

--- app/components/dashboard/MainContent.js
+++ app/components/dashboard/MainContent.js
@@ -1,9 +1,8 @@
+"use client"
+
 export default function MainContent() {
   return (
-    <div className="ml-64 mt-16 p-6 bg-[#F3F2F1] min-h-screen">
-      <div className="text-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        Main Content Area
+    <div className="ml-64 bg-[#F3F2F1] max-h-screen">
       </div>
-    </div>
   );
 }
